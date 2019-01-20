57d50dc4ae4fea3123d0733e80baac1ca898cf82
Santosh Kulkarni <santosh.nvsb@gmail.com>
2019-01-20 18:18:28 +0530

    Bug Fixes For URL

--- CardTemplate/urls.py
+++ CardTemplate/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('searchCard/', include("SearchCard.urls"))
+    path('searchCard/', include("SearchCard.urls")),
+    path('', include("SearchCard.urls"))
 ]
