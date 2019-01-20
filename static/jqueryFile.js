c89843e6a1381beb73c279646882e12d2942d420
Santosh Kulkarni <santosh.nvsb@gmail.com>
2019-01-20 17:32:17 +0530

    Added Loading Button

--- static/jqueryFile.js
+++ static/jqueryFile.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+    $("#loadingButton").css("visibility", "hidden");
     $("#data").keyup(function() {
+        $("#loadingButton").css("visibility", "visible");
         var value = $("#data").val();
         $.post("/searchCard/",
         {
@@ -8,6 +10,7 @@ $(document).ready(function() {
         function(data, status){
             $("#replace").empty();
             $("#replace").append(data);
+            $("#loadingButton").css("visibility", "hidden");
         });
     });
 });
