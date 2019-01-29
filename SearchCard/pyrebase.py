247e14895e44da9a6b06845689140946a7b294df
Santosh Kulkarni <santosh.nvsb@gmail.com>
2019-01-29 19:04:32 +0530

    Bug Fix

--- SearchCard/pyrebase.py
+++ SearchCard/pyrebase.py
@@ -11,7 +11,6 @@ import math
 from random import uniform
 import time
 from collections import OrderedDict
-import sseclient
 import threading
 import socket
 from oauth2client.service_account import ServiceAccountCredentials
